262bdf2ed0d27e2364a6976cfed5811a7fe0d6dd
nvinden <nvinden@uoguelph.ca>
2021-01-14 12:08:32 -0500

    Added convolution step on embedded vectors

--- train.py
+++ train.py
@@ -1,29 +1,40 @@
 import torch
 import torch.nn as nn
-import torch.functional as F
+import torch.nn.functional as F
 from gensim import models
 
+'''
 w = models.KeyedVectors.load_word2vec_format('/home/nvinden/ML/CNNFORNLP2014/google_embeddings/GoogleNews-vectors-negative300.bin', binary=True)
 google_vector_size = w.vector_size
 google_data_length = int(w.vectors.size/google_vector_size)
+
 
 idx_2_embeddings = torch.Tensor(w.vectors)
 idx_2_word = w.index2word
 idx_2_entity = w.index2entity
 
+'''
+
+idx_2_embeddings = torch.rand(1000, 5)
+google_vector_size = 10
+google_data_length = 1000
+
+
 class network(nn.Module):
     def __init__(self, sentence_length, embedding_dimension = 5, trained_words = True, static_words = False,
     filter_windows=(5,4,3), feature_maps=100, dropout_rate = 0.5, l2_constraint = 3, mini_batch_size=50):
         super(network, self).__init__()
-        
         self.feature_maps = feature_maps
         self.dropout_rate = dropout_rate
         self.l2_constraint = l2_constraint
         self.mini_batch_size = mini_batch_size
-
         self.trained_words = trained_words
         self.static_words = static_words
+        self.sentence_length = sentence_length
+        self.embedding_dimension = embedding_dimension
+        self.filter_windows = filter_windows
 
+        '''
         self.convolution = []
         #Defining the convolutional layer of the algorithm
         if trained_words and static_words: #Two layer architecture where one is learned and one is not
@@ -43,11 +54,32 @@ class network(nn.Module):
                 self.convolution.append([nn.Conv3d(1, sentence_length - filter_windows[i] + 1, 1) for j in range(feature_maps)])
         else:
             raise Exception("There must be at least one convolutional layer defined")
+        '''
+        self.embedding = nn.Embedding.from_pretrained(idx_2_embeddings)
+
+        self.convolution = []
+        for i in range(len(filter_windows)):
+            self.convolution.append(nn.Conv2d(1, feature_maps, (filter_windows[i], embedding_dimension)))
         
         print("returned")
 
     def forward(self, input):
-        return self.linear(input)
+        embedding = self.embedding(input)
+        convolution = []
+        for i in range(len(self.filter_windows)):
+            convolution.append(F.relu(self.convolution[i](embedding.view(self.mini_batch_size, 1, self.sentence_length, self.embedding_dimension))))
 
-net = network(20, static_words=True, trained_words=True, feature_maps=5)
-print(net)
+        return convolution
+        
+batch_size = 10
+max_sentence_length = 20
+embedding_dim = 5
+
+net = network(max_sentence_length, static_words=True, trained_words=True, feature_maps=5, mini_batch_size=batch_size, embedding_dimension=embedding_dim)
+inp = torch.LongTensor(batch_size, max_sentence_length).random_(0, google_data_length)
+print(inp.shape)
+print(inp)
+out = net(inp)
+print(out[0].shape)
+print(out[1].shape)
+print(out[2].shape)
