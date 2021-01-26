c0682bf2934abc10f4d2527cf3105638fdaa4543
nvinden <nvinden@uoguelph.ca>
2021-01-26 12:48:58 -0500

    Added train, test and val dataset

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchtext
 from gensim import models
 import numpy
 
@@ -13,12 +14,28 @@ google_data_length = int(w.vectors.size/google_vector_size)
 idx_2_embeddings = torch.Tensor(w.vectors)
 idx_2_word = w.index2word
 idx_2_entity = w.index2entity
-
 '''
+
+text = torchtext.data.Field()
+labels = torchtext.data.Field(sequential=False)
+
+train, val, test = torchtext.datasets.SST.iters(batch_size = 50, root="/home/nvinden/ML/CNNFORNLP2014/data")
+
+batch = next(iter(train))
+print(batch.text)
+print(batch.label)
+
 
 idx_2_embeddings = torch.rand(1000, 300)
 google_vector_size = 10
 google_data_length = 1000
+
+tag_list = ("positive", "negative")
+
+def make_tags(tags):
+    tag_2_idx = {tag: i for i, tag in enumerate(tags)}
+    idx_2_tag = {i: tag for i, tag in enumerate(tags)}
+    return tag_2_idx, idx_2_tag
 
 #Hyperparameters
 p = 0.5
@@ -45,6 +62,7 @@ class network(nn.Module):
         self.convolution = []
         for i in range(len(filter_windows)):
             self.convolution.append(nn.Conv2d(1, feature_maps, (filter_windows[i], embedding_dimension)))
+        self.dropout = nn.Dropout(self.dropout_rate)
         self.linear = nn.Linear(feature_maps * len(filter_windows), num_tags)
 
     def forward(self, input):
@@ -55,21 +73,29 @@ class network(nn.Module):
             convolution = F.relu(self.convolution[i](embedding.view(mini_batch_size, 1, self.sentence_length, self.embedding_dimension)))
             maxed_out = torch.max(convolution, dim = 2)[0].view(mini_batch_size, self.feature_maps)
             linear_inp = torch.cat((linear_inp, maxed_out), dim=1)
-        dropout = torch.randint(0,2,(mini_batch_size, self.embedding_dimension), requires_grad=False)
-        out = F.softmax(self.linear(dropout*linear_inp), dim=1)
+        dropout = self.dropout(linear_inp)
+        out = F.log_softmax(self.linear(dropout), dim=1)
         return out
 
-    #input is torch tensors of size number of sentences * batch size
+    #input is torch tensors of size number of sentences * batch
     #tag is a vector of shape 1 x batch size
     def cost(self, input, tag):
         y_bar = self.forward(input)
         y = torch.zeros(self.num_tags, 1)
+        y[tag_2_idx[tag]] = 1
         
+num_epochs = 100
+
 batch_size = 50
 max_sentence_length = 20
-embedding_dim = 5
+embedding_dim = 300
+
+tag_2_idx, idx_2_tag = make_tags(tag_list)
 
 net = network(max_sentence_length)
 #inp = torch.LongTensor(batch_size, max_sentence_length).random_(0, google_data_length)
 #out = net(inp)
-net.cost(torch.LongTensor(50, 20).random_(0, google_data_length), 1)
+net.cost(torch.LongTensor(50, 20).random_(0, google_data_length), "positive")
+
+for epoch in num_epochs:
+    pass
